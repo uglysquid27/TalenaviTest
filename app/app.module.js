e8ef1b13b7d85c4615bf7b2b9ef9553a813a2239
unknown <farrel.ahmad.zaidan@gmail.com>
2025-01-23 08:06:59 +0700

    update: adding tailwindcss

--- app/app.module.js
+++ app/app.module.js
@@ -1,20 +1,24 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute'])
 
-myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+myApp.config([
+  '$routeProvider',
+  '$locationProvider',
+  function ($routeProvider, $locationProvider) {
     // Define routes
     $routeProvider
-        .when('/', {
-            templateUrl: 'app/views/kanban/kanban.html',
-            controller: 'HomeController'
-        })
-        .when('/about', {
-            templateUrl: 'app/views/mainTable/mainTable.html',
-            controller: 'AboutController'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
+      .when('/', {
+        templateUrl: 'app/views/table/table.html',
+        controller: 'AboutController'
+      })
+      .when('/kanban', {
+        templateUrl: 'app/views/kanban/kanban.html',
+        controller: 'HomeController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      })
 
     // Enable HTML5 mode
-    $locationProvider.html5Mode(true);
-}]);
+    $locationProvider.html5Mode(true)
+  }
+])
